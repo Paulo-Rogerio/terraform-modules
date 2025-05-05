82bdff7060e3ddda523e79e4efb09b012e4496b1
Paulo Rogério Gomes e Silva <psilva.gomes.rogerio@gmail.com>
2025-05-04 22:10:54 -0300

    organizando codigo

--- tags/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "tags" {
-  description = "A map with all defined tags"
-  value       = local.tags
-}
-
-output "tags_as_list_of_maps" {
-  description = "A list of maps containing the defined tags"
-  value       = local.tags_as_list_of_maps
-}
