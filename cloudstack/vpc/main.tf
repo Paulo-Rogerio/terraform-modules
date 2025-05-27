1aa512953f50d69ce4b8e5138b3dab39d967a40f
Paulo Rogério Gomes e Silva <psilva.gomes.rogerio@gmail.com>
2025-05-27 07:16:28 -0300

    correcoes

--- cloudstack/vpc/main.tf
+++ cloudstack/vpc/main.tf
@@ -3,8 +3,8 @@ resource "cloudstack_vpc" "this" {
   cidr           = var.cidr
   vpc_offering   = var.vpc_offering
   zone           = var.zone
-  network_domain = var.network_domain
-  project        = var.project
+  #network_domain = var.network_domain
+  #project        = var.project
 
   tags = merge(
     {
@@ -14,4 +14,11 @@ resource "cloudstack_vpc" "this" {
   )
 }
 
-
+# Criando a rede isolada dentro da VPC
+resource "cloudstack_network" "my_network" {
+  name              = "my-network"
+  cidr              = "10.1.1.0/24"
+  network_offering  = var.vpc_offering
+  zone              = var.zone
+  vpc_id            = "98df7074-31d7-4621-9147-d8651946907c"
+}
